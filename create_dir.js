e5bb7f3da26969a7c7c30ff1a5acc2ffd864dff7
Michal Mietlinski <mietlinski.michal.priv@gmail.com>
2020-06-15 16:05:38 +0200

    Make module work with console

--- create_dir.js
+++ create_dir.js
@@ -1,17 +1,36 @@
 const fs = require('fs')
-
-const create_dir = (dirname) => {
-	console.log(dirname)
-	const route=dirname.split('/')
-	let existsingRoute=route[0]=='.'?route.splice(0,1)+'/':''
-	while(route.length!=0){
-		const pieceOfRoute=route.splice(0,1)
-		existsingRoute+=pieceOfRoute+'/'
-		if (!fs.existsSync(existsingRoute)){
-			fs.mkdirSync(existsingRoute);
+let create_dir
+if (require.main === module) {
+	create_dir = (()=>{
+		if(!process.argv[2]){
+			console.log( 'No directory passed')
+			return
 		}
+		const route=process.argv[2].split('/')
+		console.log(route)
+		let existsingRoute=route[0]=='.'?route.splice(0,1)+'/':''
+		while(route.length!=0){
+			const pieceOfRoute=route.splice(0,1)
+			existsingRoute+=pieceOfRoute+'/'
+			if (!fs.existsSync(existsingRoute)){
+				fs.mkdirSync(existsingRoute);
+			}
+		}
+	})()
+}else{
+	create_dir = (dirname) => {
+		console.log(dirname)
+		const route=dirname.split('/')
+		let existsingRoute=route[0]=='.'?route.splice(0,1)+'/':''
+		while(route.length!=0){
+			const pieceOfRoute=route.splice(0,1)
+			existsingRoute+=pieceOfRoute+'/'
+			if (!fs.existsSync(existsingRoute)){
+				fs.mkdirSync(existsingRoute);
+			}
+		}
+		
 	}
-	
 }
 module.exports = create_dir;
 
